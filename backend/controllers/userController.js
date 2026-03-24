1108c73101427d15498f09548b764c5926e65abe
Saba <revazishvilisaba37@gmail.com>
2026-03-24 19:54:32 +0400

    add signup route

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -9,7 +9,13 @@ const LoginUSer = async (req, res) => {
 //signup user
 
 const SignupUSer = async (req, res) => {
-  res.json({ mssg: "sign up user" });
+  const { email, password } = req.body;
+  try {
+    const user = await User.signup(email, password);
+    res.status(200).json({ email, user });
+  } catch (error) {
+    res.status(400).json({ error: error.message });
+  }
 };
 
 module.exports = { LoginUSer, SignupUSer };
