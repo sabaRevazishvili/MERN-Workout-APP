8715eb71f735984fc51e6c4d8ee6d6fad39efd96
საბა რევაზიშვილი <revazishvilisaba37@gmail.com>
2026-03-26 18:40:28 +0400

    add login

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -8,7 +8,16 @@ const createToken = (_id) => {
 //login user
 
 const LoginUSer = async (req, res) => {
-  res.json({ mssg: "login user" });
+  const { email, password } = req.body;
+
+  try {
+    const user = await User.login(email, password);
+    const token = createToken(user._id);
+
+    res.status(200).json({ email, token });
+  } catch (error) {
+    res.status(400).json({ error: error.message });
+  }
 };
 
 //signup user
